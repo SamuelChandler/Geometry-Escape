72ea3635dbb10a892ce7aa1b04ca00d019f31238
BlitzMK <sec180005@utdallas.edu>
2024-03-14 23:56:36 -0500

    build Final for now

--- Assets/Scripts/Scene_Manager.cs
+++ Assets/Scripts/Scene_Manager.cs
@@ -3,12 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class Scene_Manager : MonoBehaviour
 {
-    [SerializeField] public int Game_Scene_index = 1;
-
-
     public void PlayGame()
     {
-        SceneManager.LoadScene(Game_Scene_index);
+        SceneManager.LoadScene(1);
     }
 
     public void QuitGame()

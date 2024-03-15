a5ac8434458c70b3a9dc1ec56861ee458f861787
BlitzMK <sec180005@utdallas.edu>
2024-03-14 20:25:31 -0500

    energy ber now goes opaque when full

--- Assets/Scripts/EnergyBar.cs
+++ Assets/Scripts/EnergyBar.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+
 public class EnergyBar : MonoBehaviour
 {
     public static EnergyBar instance;
@@ -16,5 +18,23 @@ public class EnergyBar : MonoBehaviour
     public void SetEnergy(float CE, float ME)
     {
         fill.transform.localScale = new Vector3(CE / ME, 1f, 1f);
+    }
+
+    private void SetOpacity()
+    {
+        float fill_scale = fill.transform.localScale.x;
+        float opacity = (fill_scale - 0.8f) / 0.2f;
+        fill.color = new Color(fill.color.r, fill.color.g, fill.color.b, 1f - opacity);
+        GetComponent<Image>().color = new Color (GetComponent<Image>().color.r, GetComponent<Image>().color.g, GetComponent<Image>().color.b, 1f - opacity);
+
+    }
+
+    public void Update()
+    {
+        if (fill.transform.localScale.x > 0.8f) { SetOpacity(); }
+        else { 
+            fill.color = new Color(fill.color.r, fill.color.g, fill.color.b, 1f);
+            GetComponent<Image>().color = new Color(GetComponent<Image>().color.r, GetComponent<Image>().color.g, GetComponent<Image>().color.b, 1f); ; }
+
     }
 }
